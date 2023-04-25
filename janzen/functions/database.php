700b56ec57c89221e503cb8450dc825b36c724a8
seanpaulsesa <pauluspaul7700@gmail.com>
2023-04-25 19:50:41 +0900

    update paulus

--- janzen/functions/database.php
+++ janzen/functions/database.php
@@ -2,7 +2,7 @@
     $servername     = "localhost";
     $username       = "root";
     $password       = "";
-    $dbname         = "sacode_janzen";
+    $dbname         = "sacode_paul";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);

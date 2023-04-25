5d99a1f5153f4dbbda8f9ff328f0897af272af31
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-04-25 19:53:58 +0900

    UPDATE: janzen's

--- janzen/functions/database.php
+++ janzen/functions/database.php
@@ -2,7 +2,7 @@
     $servername     = "localhost";
     $username       = "root";
     $password       = "";
-    $dbname         = "sacode_paul";
+    $dbname         = "sacode_janzen";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);

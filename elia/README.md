cb2695bdf70b111adbd59fe0c94596410de61854
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-12 18:14:21 +0900

    UPDATE: ubah folder paul dan elia

--- elia/README.md
+++
@@ -1,2 +0,0 @@
-# php-ii-day-08-2023
-Belajar membuat CRUD dengan PHP &amp; MySQL
--- elia/partials/footer.php
+++
@@ -1,3 +0,0 @@
-            <footer class="bg-gray-300 p-5">
-                <p>Copyright &copy; 2023 - Developted by Elia</p>
-            </footer>
